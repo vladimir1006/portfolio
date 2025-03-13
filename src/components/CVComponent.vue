7f667d7ad58aa4ef4d52f21391d65c3c7a74bc07
vadim.ablancourt <vadimablancourt683@gmail.com>
2025-03-13 16:49:13 +0100

    snake iframe maybe working...

--- src/components/CVComponent.vue
+++ src/components/CVComponent.vue
@@ -1,7 +1,11 @@
+<script setup>
+import cvPdf from '@/assets/Ablancourt-3.pdf';
+</script>
+
 <template>
     <div class="cv-component">
         <h1>Curriculum Vitae</h1>
-        <a href="@/assets/Ablancourt-3.pdf" download>Download CV</a>
+        <a :href="cvPdf" download>Download CV</a>
     </div>
 </template>
 
--- src/components/SideProjectTemplate.vue
+++ src/components/SideProjectTemplate.vue
@@ -1,3 +1,7 @@
+<script setup>
+    import IframeSnake from '@/assets/iframe/snake.html';
+</script>
+
 <template>
 
     <div class="side-project-template">
@@ -7,7 +11,14 @@
         <p>Root-me profile: </p>
     </div>
     <div class="side-project-template">
-        <img src="../assets/test.png" style="width: 100px;"/>
+        
+        <iframe :src="IframeSnake" 
+                frameborder="0" 
+                width="400" 
+                height="400" 
+                style="overflow: hidden;" 
+                scrolling="no">
+        ></iframe>
         <p>parler du snake auto solver, mettre le iframe (a voir si ca marche)</p>
     </div>
 </template>
