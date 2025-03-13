f394dca4a17469b3a390231feaa85fa84115497c
vadim.ablancourt <vadimablancourt683@gmail.com>
2025-03-13 14:15:26 +0100

    maybe...

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 import App from './App.vue'
 
 createApp(App).mount('#app')
